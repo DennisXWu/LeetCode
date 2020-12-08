65ba1f616d22c12c2854740bced1b394fae96222
wxg1990 <denniswu19900627@yahoo.com>
2020-12-09 00:13:41 +0800

    12.8

---
+++ src/main/java/offer/Offer28.java
@@ -0,0 +1,33 @@
+package offer;
+
+/**
+ * Created by wxg on 2020/12/8.
+ */
+public class Offer28 {
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return helper(root.right, root.left);
+    }
+
+    private boolean helper(TreeNode right, TreeNode left) {
+        if (right == null && left == null) return true;
+        if (right == null && left != null) return false;
+        if (left == null && right != null) return false;
+
+        return right.val == left.val && helper(right.left, left.right) && helper(right.right, left.left);
+    }
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
