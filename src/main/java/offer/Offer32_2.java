f6504eb36a23b3d8807e8ca535fb992b28c3717f
wxg1990 <denniswu19900627@yahoo.com>
2020-12-10 00:41:43 +0800

    12.9

---
+++ src/main/java/offer/Offer32_2.java
@@ -0,0 +1,41 @@
+package offer;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Created by wxg on 2020/12/10.
+ */
+public class Offer32_2 {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> list = new ArrayList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> subList = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                subList.add(node.val);
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            list.add(subList);
+        }
+        return list;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
